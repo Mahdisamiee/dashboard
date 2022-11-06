83c141545a7ad69551a5ffbb40ea90e4e026fd42
m_lind <md.samieenia@gmail.com>
2022-11-06 22:36:46 +0330

    add dark mode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useColorMode, ColorModeContext } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import TopBar from "./components/global/TopBar/TopBar";
+
 function App() {
+  const [theme, colorMode] = useColorMode();
   return (
-    <div className="app">
-      Hello from Me
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <TopBar />
+            {/* min 55 from video */}
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
